5e83e11f792e361510f7033647252b9d3b9e9b32
AnastasiaTsey <48406615+AnastasiaTsey@users.noreply.github.com>
2019-06-05 10:38:12 +0300

    Update laba 2.4..py

--- laba 2.4..py
+++ laba 2.4..py
@@ -4,11 +4,11 @@ for i in range (0, n):
     l.append(int(input('Введите элемент списка: ')))
 print (l)
     #del range[4,5,6,7,8]
-l.remove(4)
+
 l.remove(5)
 l.remove(6)
 l.remove(7)
-l.remove(8)
+
 print(l)
 
 for i in range (0, 2):
